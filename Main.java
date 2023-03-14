b52bcd1c3b80b99540b238d9ebc1fcdc3558dc77
Stephanie Abutu <stephabuts@gmail.com>
2023-03-14 01:19:10 +0100

    Task 14: Introduction to Java Programming Language

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world! This is my first Java project. Shout out to Testify and Team Newman");
+
+                System.out.println("Being at Testify Academy has been tasking and yet interesting");
+        System.out.println("The journey which began a day now has weeks and months behind it.");
+        System.out.println("This experience has been both frustrating and exciting when new knowledge is gained");
+System.out.println("This is me to the world of software testing!");
+
+
+        }
+    }
+
+
+
+
